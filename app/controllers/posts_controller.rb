0b2f5449c642d141a91fea48251e37fcb6115a69
Eric A. Wood <eric.wood.ea@gmail.com>
2013-11-01 09:14:16 -0500

    Implemented the create and new actions for support posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,6 +12,13 @@ class PostsController < ApplicationController
   end #Automatically loads app/view/posts/new.html.erb
   
   def create
-    
+    @post = Post.new(params[:post])
+    if @post.save
+      #redirect to the index actions
+      redirect_to :action => :index
+    else
+      #load up the view associated with the new action
+      render :action => :new
+    end
   end #On success redirects to index, else uses new.html.erb
 end
