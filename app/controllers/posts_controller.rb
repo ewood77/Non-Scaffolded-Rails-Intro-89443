dfb6a3d99bd71de4471c86e51c553739e9ae2411
Eric A. Wood <eric.wood.ea@gmail.com>
2013-11-01 08:33:38 -0500

    Added routes and preparing for creating posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,12 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end #Automatically loads app/views/posts/show.html.erb
+  
+  def new
+    @post = Post.new
+  end #Automatically loads app/view/posts/new.html.erb
+  
+  def create
+    
+  end #On success redirects to index, else uses new.html.erb
 end
